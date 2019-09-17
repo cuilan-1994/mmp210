2aa1aa790c68b2875f7f3f39962f75bf4a17719f
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-09-17 13:30:26 -0400

    ok

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,10 +1,25 @@
+function sandwich (fruit){
+console.log ("i want a"+ fruit )}
 function setup() { 
-    createCanvas(600,400);
-}
+ createCanvas(600, 400);
+	background("lightblue");
+	noStroke();
+	fill("plum");
+	ellipse(300, 200,  300); // face
 
-function draw() {
-    background("lightblue");
-    circle(300,200,100);
-    ellipse(400,200,50,100);
-    circle(10, 10, 10);
+	fill("yellow");
+	ellipse(400, 170,100, 50); // right eye
+	ellipse(200, 170, 100,50); // left eye
+
+	fill("red");
+	rect(270, 300, 50, 10, 30); // mouth	
+    
+    fill("green");
+	ellipse(400, 170, 40); // right eye
+    ellipse(200, 170, 40); // left eye
+    
+    fill("Green");
+    strokeWeight(9);
+	ellipse(300, 220, 10, 20); // right eye
 }
+ 
