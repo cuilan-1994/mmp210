2f79390a3dceb112bbfd7435d86629cbaf6f243b
Lan.cui <lancui@Lans-MacBook-Pro.local>
2019-09-24 12:39:19 -0400

    ok

--- variables/sketch.js
+++ variables/sketch.js
@@ -8,21 +8,23 @@ function setup() {
 	
 	stroke(255, 168, 213);
 	strokeWeight(2);
+    var neckSize = 290;
+    
 	fill(255, 168, 213);
 	rect(130, 250, 140, 300, 150); //clothes
 
 	fill(230, 202, 181);
-	quad(170, 190, 190, 290, 220, 290, 240, 130);//neck
+	quad(170, 190, 190, neckSize, 220, neckSize, 240, 130);//neck
   
 	noStroke();
 	fill(199, 178, 163); 
-	quad(218, 270, 190, 290, 220, 290, 225, 240);//neck shadow
+	quad(218, 270, 190, neckSize, 220, neckSize, 225, 240);//neck shadow
 
 
 	//hair
-	fill(48, 22, 7);
-	stroke(48, 22, 7);
-	
+   var hairColor =(48, 22, 7)
+	fill(hairColor);
+	stroke(hairColor);
 	ellipse(185, 160, 150, 150);
 	ellipse(210, 150, 170, 170);
   quad(110, 250, 300, 250, 290, 170, 110, 170);
@@ -32,8 +34,11 @@ function setup() {
 	ellipseMode(CENTER);
 	strokeWeight(2);
 	fill(230, 202, 181);
-	ellipse(120, 190, 30, 50); //ears
-	ellipse(280, 190, 30, 50); //ears
+    var a =190
+    var b =30
+    var c =50 
+	ellipse(120, a, b, c); //ears
+	ellipse(280, a, b, c); //ears
 
 	stroke(0);
 	fill(230, 202, 181);
@@ -48,14 +53,15 @@ function setup() {
     
     fill(252, 252, 252);
 	stroke(0);
-	ellipse(160, 180, 50, 25); //eyes
-	ellipse(240, 180, 50, 25); 
+     var eyeSize =180
+	ellipse(160, eyeSize, 50, 25); //eyes
+	ellipse(240, eyeSize, 50, 25); 
     
     
     fill(0);
 	stroke(0);
-	ellipse(160, 180, 20); //eyes
-	ellipse(240, 180, 20); 
+    ellipse(160, eyeSize, 20); //eyes
+	ellipse(240, eyeSize, 20); 
     
 	line(190, 190, 200, 200); //nose
 	line(210, 220, 185, 220); //mouth
@@ -66,9 +72,10 @@ function setup() {
 
 	noFill();
 	stroke(134, 89, 45, 90);
+    var eyebagSize= 12
 	strokeWeight(3);
-	arc(160,198, 12, 1, 0, PI); //eyebags
-	arc(240, 198, 12, 1, 0, PI);
+	arc(160,198, eyebagSize, 1, 0, PI); //eyebags
+	arc(240, 198, eyebagSize, 1, 0, PI);
 
 	
 }
