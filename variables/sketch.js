e2f54186cff74c60226cc020b87563d769cf605f
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-09-24 15:45:12 -0400

    ok

--- variables/sketch.js
+++ variables/sketch.js
@@ -5,65 +5,66 @@ function setup() {
 	background("lightblue");
 	noStroke();
 	//body
-	
 	stroke(255, 168, 213);
 	strokeWeight(2);
-    var neckSize = 290;
-    
+  
 	fill(255, 168, 213);
 	rect(130, 250, 140, 300, 150); //clothes
 
 	fill(230, 202, 181);
-	quad(170, 190, 190, neckSize, 220, neckSize, 240, 130);//neck
+	quad(170, 190, 190, 290, 220, 290, 240, 130);//neck
   
 	noStroke();
 	fill(199, 178, 163); 
-	quad(218, 270, 190, neckSize, 220, neckSize, 225, 240);//neck shadow
+	quad(218, 270, 190, 290, 220, 290, 225, 240);//neck shadow
 
 
 	//hair
    var hairColor =(48, 22, 7)
+   var headColor =(230, 202, 181)
+   var faceColor =(230, 202, 181)
+    var earSize =30
+    var earsSize=50 
+    var eyebagSize= 12
+    var eyeY =180
+    var eyeX =160
+   var x=100
+   var y=100
 	fill(hairColor);
 	stroke(hairColor);
-	ellipse(185, 160, 150, 150);
-	ellipse(210, 150, 170, 170);
+	ellipse(x+85, y+60, 150, 150);
+	ellipse(x+110, y+50, 170, 170);
   quad(110, 250, 300, 250, 290, 170, 110, 170);
 
 
 	//head
 	ellipseMode(CENTER);
 	strokeWeight(2);
-	fill(230, 202, 181);
-    var a =190
-    var b =30
-    var c =50 
-	ellipse(120, a, b, c); //ears
-	ellipse(280, a, b, c); //ears
+	fill(headColor);
+	ellipse(x+20, y+90, earSize, earsSize); //ears
+	ellipse(x+180, y+90, earSize, earsSize); //ears
 
 	stroke(0);
-	fill(230, 202, 181);
-	ellipse(200, 180, 160, 170); //face
+	fill(faceColor);
+	ellipse(x+100, y+80, 160, 170); //face
 
 	//detail & shadow
 	noStroke();
 	fill(250, 220, 197);
-	ellipse(202, 180, 155, 166); //face
+	ellipse(x+102, y+80, 155, 166); //face
 
 	
     
     fill(252, 252, 252);
 	stroke(0);
-     var eyeSize =180
-    var y=50
-   
-	ellipse(160, eyeSize, y, 25); 
-	ellipse(240, eyeSize, y, 25); 
+    ellipse(eyeX, eyeY, earsSize, 25); //eye
+	ellipse(eyeX+80, eyeY, earsSize, 25); 
     
     
     fill(0);
 	stroke(0);
-    ellipse(160, eyeSize, 20); //eyes
-	ellipse(240, eyeSize, 20); 
+    ellipse(eyeX, eyeY, 20); //eyes
+	ellipse(240, eyeY, 20); 
     
 	line(190, 190, 200, 200); //nose
 	line(210, 220, 185, 220); //mouth
@@ -74,7 +75,6 @@ function setup() {
 
 	noFill();
 	stroke(134, 89, 45, 90);
-    var eyebagSize= 12
 	strokeWeight(3);
 	arc(160,198, eyebagSize, 1, 0, PI); //eyebags
 	arc(240, 198, eyebagSize, 1, 0, PI);
