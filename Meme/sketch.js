003a5efcd509bfc6b4099e132f6b0905cba8106d
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-10-17 15:36:09 -0400

    Update sketch.js

--- Meme/sketch.js
+++ Meme/sketch.js
@@ -5,9 +5,19 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(940, 680);
+    createCanvas(500, 500);
+  
 }
 
-function draw(){
-	image(img, 0, 0);
-}
+function draw(){ 
+  image(img, 0, 0, width/2, height/2);
+    image(img, width/2, 0, width/2, height/2);    
+	
+fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont("Courier New");
+    text("so tired",  width/2, height/8);
+
+    }
+    
