c91a84a995a68336492a1770d79664d2030a7cf5
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-10-24 14:32:34 -0400

    ok

--- interactive meme/sketch.js
+++ interactive meme/sketch.js
@@ -1,13 +1,41 @@
-var img;
+
+var dog1, dog2;
 
 function preload() {
-	img = loadImage("img/download.jpeg");
-}
+	dog1 = loadImage("img/download.jpeg");
+    dog2 = loadImage("img/dog.jpg");
 
+}
 function setup() {
-    createCanvas(940, 680);
+    createCanvas(500, 500);
+  
 }
+function draw() {
+	background(220);
+	rectMode(CENTER);
+    
+    if (mouseX > width/2) {
+        image(dog1, 0, 0, width/2, height);
+        image(dog2, width/2, 0, width/2, height);
+    } else {
+        image(dog2, 0, 0, width/2, height);
+        image(dog1, width/2, 0, width/2, height);
+    }
 
-function draw(){
-	image(img, 0, 0);
+	if (mouseIsPressed) {
+		if (mouseX > width/2) {
+            fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont("Courier New");
+    text("MEME",  width/2, height/2);
+			;
+		} else {
+			fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont("Courier New");
+    text("so tired",  width/2, height/8);
+		}
+	}	
 }
