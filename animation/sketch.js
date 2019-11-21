f74d2b111191e0cd3b034e53a683f983cc6c22f8
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-11-21 14:37:08 -0500

    ok

--- animation/sketch.js
+++ animation/sketch.js
@@ -1,41 +1,16 @@
-
-var dog1, dog2;
-
-function preload() {
-	dog = loadImage("img/download.jpeg");
-    cat = loadImage("img/cat copy.jpg");
-
-}
 function setup() {
-    createCanvas(500, 500);
-  
+	createCanvas(640, 360);
+	textSize(100);
+	textFont('Comic Sans MS');
+	textAlign(CENTER, CENTER);
+	fill('plum');
 }
 function draw() {
-	background(220);
-	rectMode(CENTER);
-    
-    if (mouseX > width/2) {
-        image(dog, 0, 0, width/2, height);
-        image(cat, width/2, 0, width/2, height);
-    } else {
-        image(cat, 0, 0, width/2, height);
-        image(dog, width/2, 0, width/2, height);
-    }
+	background(0);
+	
+	translate(width/2, height/2);
+	var r = frameCount / 1000 * PI;
+	shearX(r);
 
-	if (mouseIsPressed) {
-		if (mouseX > width/2) {
-            fill("#9332a8");
-    textAlign(CENTER, CENTER);
-    textSize(100);
-    textFont("Courier New");
-    text("MEME",  width/2, height/2);
-			;
-		} else {
-			fill(0);
-    textAlign(CENTER, CENTER);
-    textSize(100);
-    textFont("Courier New");
-    text("so tired",  width/2, height/8);
-		}
-	}	
+	text('so tried', 0, 0);
 }
