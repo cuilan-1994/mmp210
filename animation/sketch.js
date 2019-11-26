8d54cc7d0fee701d8599ac00acb43e7fd907388d
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2019-11-26 11:55:55 -0500

    ok

--- animation/sketch.js
+++ animation/sketch.js
@@ -1,16 +1,34 @@
+
+
+
+
+function preload() {
+	dog = loadImage("img/download.jpeg");
+    cat = loadImage("img/cat copy.jpg");
+
+}
 function setup() {
-	createCanvas(640, 360);
-	textSize(100);
-	textFont('Comic Sans MS');
-	textAlign(CENTER, CENTER);
-	fill('plum');
+    createCanvas(500, 500);
+  
 }
 function draw() {
-	background(0);
-	
-	translate(width/2, height/2);
+	background(220);
+	rectMode(CENTER);
+    
+    if (mouseX > width/2) {
+        image(dog, 0, 0, width/2, height);
+        image(cat, width/2, 0, width/2, height);
+    } else {
+        image(cat, 0, 0, width/2, height);
+        image(dog, width/2, 0, width/2, height);
+    }
+
+    translate(width/2, height/2);
 	var r = frameCount / 1000 * PI;
 	shearX(r);
-
+    textSize(70);
+	textFont('Comic Sans MS');
+    textAlign(CENTER, CENTER);
+	fill('plum');
 	text('so tried', 0, 0);
 }
